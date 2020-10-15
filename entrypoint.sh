057079a97ceed4869ae5ebe1b34861661925c500
nghiach <chnghia@gmail.com>
2020-10-15 09:42:20 +0700

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,4 +19,4 @@ npm run start
 # Deploy it on Firebase
 firebase functions:config:set oauth.client_id=${OAUTH_CLIENT_ID} oauth.client_secret=${OAUTH_CLIENT_SECRET}
 firebase use --token "${FIREBASE_TOKEN}"
-firebase deploy --non-interactive --token "${FIREBASE_TOKEN}"
+firebase deploy --non-interactive --token "${FIREBASE_TOKEN}" --only hosting
